598cf4c89aef8abdf12643fc5b2a965e22c234ac
itolimaesther <itolimaesther@gmail.com>
2021-03-30 00:12:09 +0100

    added active link

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -2,6 +2,9 @@ import React from "react";
 import CloseIcon from "@material-ui/icons/Close";
 
 function Sidebar({mode}) {
+
+    let url="#";
+
   return (
     <aside className="sidenav">
       <div className="sidenav__close-icon">
@@ -13,19 +16,19 @@ function Sidebar({mode}) {
       <div className="list__wrap">
         {mode === "app" ? (
           <ul className={mode === "app" ? "appnav__list-show" : ""}>
-            <li className="appnav__list-item">Entry1</li>
-            <li className="appnav__list-item">Entry2</li>
-            <li className="appnav__list-item">Entry3</li>
-            <li className="appnav__list-item">Entry4</li>
-            <li className="appnav__list-item">Entry5</li>
+            <li className="appnav__list-item"> <a href={url}>Entry1</a> </li>
+            <li className="appnav__list-item"> <a href={url}>Entry2</a> </li>
+            <li className="appnav__list-item"> <a href={url}>Entry3</a> </li>
+            <li className="appnav__list-item"> <a href={url}>Entry4</a> </li>
+            <li className="appnav__list-item"> <a href={url}>Entry5</a> </li>
           </ul>
         ) : (
           <ul className="adminnav__list">
-            <li className="adminnav__list-item">AdminEntry1</li>
-            <li className="adminnav__list-item">AdminEntry2</li>
-            <li className="adminnav__list-item">AdminEntry3</li>
-            <li className="adminnav__list-item">AdminEntry4</li>
-            <li className="adminnav__list-item">AdminEntry5</li>
+            <li className="adminnav__list-item"><a href={url}>AdminEntry1</a></li>
+            <li className="adminnav__list-item"><a href={url}>AdminEntry2</a></li>
+            <li className="adminnav__list-item"><a href={url}>AdminEntry3</a></li>
+            <li className="adminnav__list-item"><a href={url}>AdminEntry4</a></li>
+            <li className="adminnav__list-item"><a href={url}>AdminEntry5</a></li>
           </ul>
         )}
       </div>
