c9460ab2976b0bb290cd042f9cb54b0f1f7981a0
itolimaesther <itolimaesther@gmail.com>
2021-03-29 23:00:31 +0100

    added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-
+import React, { useState } from "react";
 import './App.css';
+import MenuIcon from "@material-ui/icons/Menu";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
+  const [mode, setMode] = useState("app");
+
+  
+  const handleChange = (e) => {
+    setMode(e.target.value);
+  };
+
   return (
-    <div className="App">
-      
+    <div className="grid-container">
+      <div className="menu-icon">
+        <MenuIcon className="header__menu" />
+      </div>
+
+      {/* header */}
+
+      <header className="header">
+        <div className="toggle__menu">
+        <label className="app__label" htmlFor="app">
+            <input
+              id="admin"
+              type="radio"
+              value="app"
+              name="app"
+              checked={mode === "app"}
+              onChange={handleChange}
+            />
+            App mode
+          </label>
+          <label className="admin__label" htmlFor="admin">
+            <input
+              id="admin"
+              type="radio"
+              value="admin"
+              name="admin"
+              checked={mode === "admin"}
+              onChange={handleChange}
+            />
+            Admin mode
+          </label>
+        </div>
+      </header>
     </div>
   );
 }
